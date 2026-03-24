555c32f5d13899ab643028b7fa33f2228720821b
SorenFall <andrefilipe.office@gmail.com>
2026-03-24 02:08:52 -0300

    Segundo Commit

--- java_exe_01/src/java_exe_05.java
+++ java_exe_01/src/java_exe_05.java
@@ -4,9 +4,8 @@ void main( ) {
 	Locale.setDefault(Locale.US);
 	Scanner sc = new Scanner(System.in);
 	
-	int c1, c2, q1, q2;
+	int c2, q1, q2;
 	double p1, p2, pt;
-	c1 = sc.nextInt();
 	q1 = sc.nextInt();
 	p1 = sc.nextDouble();
 	
@@ -14,7 +13,7 @@ void main( ) {
 	q2 = sc.nextInt();
 	p2 = sc.nextDouble();
 	
-	pt = p1 * q1 + p2 * q2;
+	pt = p1 * q1 + p2 * q2 + c2;
 	
 	System.out.printf("VALOR A PAGAR: R$ %.2f", pt);
 	
